7c18df757433be0d4d2d339053a3c776672847a4
Matt Buck <techpeace@gmail.com>
2013-06-24 09:09:13 -0500

    Add Movie model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130621124750) do
+ActiveRecord::Schema.define(:version => 20130624132630) do
+
+  create_table "movies", :force => true do |t|
+    t.string   "title"
+    t.integer  "star_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "stars", :force => true do |t|
     t.string   "first_name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,38 +11,69 @@ stars = [
     last_name:  'Lawrence',
     city:       'Santa Monica',
     state:      'CA',
-    birthdate:  Date.parse('Aug 15 1990')
+    birthdate:  Date.parse('Aug 15 1990'),
+    movies: [
+      'The Hunger Games',
+      'Silver Linings Playbook',
+      'Winter\'s Bone'
+    ]
   },
   {
     first_name: 'Joseph',
     last_name:  'Gordon-Levitt',
     city:       'Los Angeles',
     state:      'CA',
-    birthdate:  Date.parse('Feb 17 1981')
+    birthdate:  Date.parse('Feb 17 1981'),
+    movies: [
+      'Inception',
+      'The Dark Knight Rises',
+      'Looper'
+    ]
   },
   {
     first_name: 'Kristen',
     last_name:  'Stewart',
     city:       'Los Angeles',
     state:      'CA',
-    birthdate:  Date.parse('Aug 15 1990')
+    birthdate:  Date.parse('Aug 15 1990'),
+    movies: [
+      'Twilight',
+      'Snow White and the Huntsmen'
+    ]
   },
   {
     first_name: 'Channing',
     last_name:  'Tatum',
     city:       'Los Angeles',
     state:      'CA',
-    birthdate:  Date.parse('April 26 1980')
+    birthdate:  Date.parse('April 26 1980'),
+    movies: [
+      '21 Jump Street'
+    ]
   },
   {
     first_name: 'Anne',
     last_name:  'Hathaway',
     city:       'New York',
     state:      'NY',
-    birthdate:  Date.parse('Nov 12 1982')
+    birthdate:  Date.parse('Nov 12 1982'),
+    movies: [
+      'Les Misérables'
+    ]
+  },
+  {
+    first_name: 'Chris',
+    last_name:  'Himsworth',
+    movies: []
   }
 ]
 
+Star.delete_all
+
 stars.each do |star_attrs|
-  Star.create star_attrs
+  movies = star_attrs.delete(:movies)
+  s = Star.create star_attrs
+  #movies.each do |movie|
+  #  s.movies.create title: movie
+  #end
 end
